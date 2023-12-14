062534b6160ab230b5bd7e7ad34ecae687a09693
Oli Hock <oliver.hock@gmail.com>
2023-12-14 13:30:07 +0100

    feat(api): Implement pagination in products endpoint
    - Add `Pageable` parameter to `getAllProducts` method in `SearchFunctionalityController`
    - Modify `productRepository.findAll(pageable)` to return `Page<ProductEntity>`
    - Update return type of `getAllProducts` method to `ResponseEntity<Page<ProductEntity>>`
    
    This change allows the `getAllProducts` endpoint to support pagination. The `Pageable` parameter allows clients to specify the page number, page size, and sorting criteria. The endpoint now returns a `ResponseEntity` containing a `Page` of `ProductEntity` objects, instead of a list. This ensures that only the requested page of products is returned to the client.

--- src/main/java/de/ithoc/atmosphere/service/searchandfiltering/api/SearchFunctionalityController.java
+++ src/main/java/de/ithoc/atmosphere/service/searchandfiltering/api/SearchFunctionalityController.java
@@ -1,15 +1,14 @@
 package de.ithoc.atmosphere.service.searchandfiltering.api;
 
-import de.ithoc.atmosphere.service.searchandfiltering.model.Product;
 import de.ithoc.atmosphere.service.searchandfiltering.repository.CategoryEntity;
 import de.ithoc.atmosphere.service.searchandfiltering.repository.CategoryRepository;
 import de.ithoc.atmosphere.service.searchandfiltering.repository.ProductEntity;
 import de.ithoc.atmosphere.service.searchandfiltering.repository.ProductRepository;
-import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,41 +19,31 @@ public class SearchFunctionalityController {
 
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
-    private final ModelMapper modelMapper;
 
-    public SearchFunctionalityController(ProductRepository productRepository, CategoryRepository categoryRepository, ModelMapper modelMapper) {
+    public SearchFunctionalityController(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
-        this.modelMapper = modelMapper;
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
+    public ResponseEntity<Page<ProductEntity>> getAllProducts(Pageable pageable) {
 
-        List<ProductEntity> productEntities = productRepository.findAll();
+        Page<ProductEntity> productEntityPage = productRepository.findAll(pageable);
 
-        List<Product> products = productEntities.stream()
-                .map(productEntity -> modelMapper.map(productEntity, Product.class))
-                .toList();
-
-        return ResponseEntity.ok(products);
+        return ResponseEntity.ok(productEntityPage);
     }
 
     @GetMapping(params = "search")
-    public ResponseEntity<List<Product>> searchProducts(@RequestParam String search) {
+    public ResponseEntity<Page<ProductEntity>> searchProducts(Pageable pageable, @RequestParam String search) {
 
-        List<ProductEntity> productEntities =
-                productRepository.findByNameContainingOrDescriptionContaining(search, search);
+        Page<ProductEntity> productEntityPage =
+                productRepository.findByNameContainingOrDescriptionContaining(pageable, search, search);
 
-        List<Product> products = productEntities.stream()
-                .map(productEntity -> modelMapper.map(productEntity, Product.class))
-                .toList();
-
-        return ResponseEntity.ok(products);
+        return ResponseEntity.ok(productEntityPage);
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+    public ResponseEntity<ProductEntity> getProductById(@PathVariable String id) {
 
         UUID uuid = UUID.fromString(id);
         ProductEntity productEntity = productRepository.findById(uuid).orElse(null);
@@ -62,39 +51,29 @@ public class SearchFunctionalityController {
             return ResponseEntity.notFound().build();
         }
 
-        Product product = modelMapper.map(productEntity, Product.class);
-
-        return ResponseEntity.ok(product);
+        return ResponseEntity.ok(productEntity);
     }
 
     @GetMapping(params = "category")
-    public ResponseEntity<List<Product>> searchProductsByCategory(@RequestParam String category) {
+    public ResponseEntity<Page<ProductEntity>> searchProductsByCategory(Pageable pageable, @RequestParam String category) {
 
         Optional<CategoryEntity> categoryEntityOptional = categoryRepository.findByName(category);
         if (categoryEntityOptional.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
-        List<ProductEntity> productEntities = productRepository.findByCategory(categoryEntityOptional.get());
+        Page<ProductEntity> productEntityPage = productRepository.findByCategory(pageable, categoryEntityOptional.get());
 
-        List<Product> products = productEntities.stream()
-                .map(productEntity -> modelMapper.map(productEntity, Product.class))
-                .toList();
-
-        return ResponseEntity.ok(products);
+        return ResponseEntity.ok(productEntityPage);
     }
 
     @GetMapping(params = {"fromPrice", "toPrice"})
-    public ResponseEntity<List<Product>> searchProductsByPriceRange(
-            @RequestParam double fromPrice, @RequestParam double toPrice) {
+    public ResponseEntity<Page<ProductEntity>> searchProductsByPriceRange(
+            Pageable pageable, @RequestParam double fromPrice, @RequestParam double toPrice) {
 
-        List<ProductEntity> productEntities = productRepository.findByPriceBetween(fromPrice, toPrice);
+        Page<ProductEntity> productEntityPage = productRepository.findByPriceBetween(pageable, fromPrice, toPrice);
 
-        List<Product> products = productEntities.stream()
-                .map(productEntity -> modelMapper.map(productEntity, Product.class))
-                .toList();
-
-        return ResponseEntity.ok(products);
+        return ResponseEntity.ok(productEntityPage);
     }
 
 }
--- src/main/java/de/ithoc/atmosphere/service/searchandfiltering/repository/ProductRepository.java
+++ src/main/java/de/ithoc/atmosphere/service/searchandfiltering/repository/ProductRepository.java
@@ -1,17 +1,19 @@
 package de.ithoc.atmosphere.service.searchandfiltering.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
 
     // SELECT * FROM PRODUCT WHERE name like '%Pro%' OR description like '%Pro%';
-    List<ProductEntity> findByNameContainingOrDescriptionContaining(String searchName, String searchDescription);
+    Page<ProductEntity> findByNameContainingOrDescriptionContaining(
+            Pageable pageable, String searchName, String searchDescription);
 
-    List<ProductEntity> findByCategory(CategoryEntity category);
+    Page<ProductEntity> findByCategory(Pageable pageable, CategoryEntity category);
 
-    List<ProductEntity> findByPriceBetween(double fromPrice, double toPrice);
+    Page<ProductEntity> findByPriceBetween(Pageable pageable, double fromPrice, double toPrice);
 
 }
